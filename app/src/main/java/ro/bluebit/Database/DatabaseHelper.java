ba741f29caab8403cec37186dd0e86b87540e250
Marian <stanciu.m@gmail.com>
2019-10-11 12:28:43 +0300

    constructor finalizat

--- app/src/main/java/ro/bluebit/Database/DatabaseHelper.java
+++ app/src/main/java/ro/bluebit/Database/DatabaseHelper.java
@@ -10,7 +10,7 @@ import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public final static int VERSIUNE_BAZA_DE_DATE =10;
+    public final static int VERSIUNE_BAZA_DE_DATE =11;
 
     public DatabaseHelper(@Nullable Context context) {
         super(context, Constructor.DATABASE_NAME, null, VERSIUNE_BAZA_DE_DATE);
@@ -21,13 +21,24 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
 
         db.execSQL(Constructor.SQL_CREAZA_TABEL_UTILIZATORI);
+        db.execSQL(Constructor.SQL_CREAZA_TABEL_ANTET_TRIMITERI);
+        db.execSQL(Constructor.SQL_CREAZA_TABEL_POZITII_TRIMITERI);
+        db.execSQL(Constructor.SQL_CREAZA_TABEL_INCARC_DESCARC);
+        db.execSQL(Constructor.SQL_CREAZA_TABEL_P_LUCRU);
+        db.execSQL(Constructor.SQL_CREAZA_TABEL_PACHETE);
+        db.execSQL(Constructor.SQL_CREAZA_TABEL_PLAJA_COD);
+        db.execSQL(Constructor.SQL_CREAZA_TABEL_RUTE);
+        db.execSQL(Constructor.SQL_CREAZA_TABEL_RUTE_P_LUCRU);
+        db.execSQL(Constructor.SQL_CREAZA_TABEL_TIPURI);
+
+
         Log.e("Baza de date", "Tabela " + Constructor.TabelaUtilizatorPin.NUME_TABEL + " a fost creata");
         String sqlSir="";
-        sqlSir="INSERT INTO Tabela_utilizatori (_id, nume, pin, nivel_acces,_id_departament) VALUES   (1, 'Marian', 111111, 1, 1)";
+        sqlSir="INSERT INTO Tabela_utilizatori (id_utilizator, nume, pin, nivel_acces,id_departament) VALUES   (1, 'Marian', 111111, 1, 1)";
         db.execSQL(sqlSir);
-        sqlSir="INSERT INTO Tabela_utilizatori (_id, nume, pin, nivel_acces,_id_departament) VALUES   (2, 'Adrian', 222222, 2, 2)";
+        sqlSir="INSERT INTO Tabela_utilizatori (id_utilizator, nume, pin, nivel_acces,id_departament) VALUES   (2, 'Adrian', 222222, 2, 2)";
         db.execSQL(sqlSir);
-        sqlSir="INSERT INTO Tabela_utilizatori (_id, nume, pin, nivel_acces,_id_departament) VALUES   (3, 'Vasile', 333333, 4, 2)";
+        sqlSir="INSERT INTO Tabela_utilizatori (id_utilizator, nume, pin, nivel_acces,id_departament) VALUES   (3, 'Vasile', 333333, 4, 2)";
         db.execSQL(sqlSir);
     }
 
