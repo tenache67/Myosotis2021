20558f3f29b41ef293825761845a3011cbd88027
Marian <stanciu.m@gmail.com>
2019-10-30 10:37:41 +0200

    tentativa esuata

--- app/src/main/java/ro/bluebit/Database/DatabaseHelper.java
+++ app/src/main/java/ro/bluebit/Database/DatabaseHelper.java
@@ -12,7 +12,7 @@ import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public final static int VERSIUNE_BAZA_DE_DATE =21;
+    public final static int VERSIUNE_BAZA_DE_DATE =22;
 
     public DatabaseHelper(@Nullable Context context) {
         super(context, Constructor.DATABASE_NAME, null, VERSIUNE_BAZA_DE_DATE);
--- app/src/main/java/ro/bluebit/MainActivity.java
+++ app/src/main/java/ro/bluebit/MainActivity.java
@@ -8,18 +8,26 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import ro.bluebit.Database.Constructor;
 import ro.bluebit.Database.DatabaseHelper;
+import ro.bluebit.UTILITARE.LogicaVerificari;
 import ro.bluebit.UTILITARE.SelectieInitialaActivity;
+
+import static ro.bluebit.R.layout.support_simple_spinner_dropdown_item;
 
 public class MainActivity extends AppCompatActivity {
     Button acceseaza;
     EditText parola;
     DatabaseHelper myDb;
+    AutoCompleteTextView punctLucru;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,40 +36,53 @@ public class MainActivity extends AppCompatActivity {
         myDb = new DatabaseHelper(this);
 // crearea datelor de proba - am pus aici ca sa ruleze de fiecate data la pornire
         myDb.dateProba();
-
+        punctLucru = findViewById(R.id.AC_PunctLucru);
         Toolbar toolbarSimplu = findViewById(R.id.toolbarSimplu);
         setSupportActionBar(toolbarSimplu);
         toolbarSimplu.setSubtitle("Logare in aplicatie");
 
-        parola = findViewById(R.id.editTextPIN);
+//afisare autocomplete si obtinerea id punct de lucru pentru intent
+        PopulareAutocomplete();
+        acceseazaAplicatia();
+
+
+
+
+    }
+
+
+    public void acceseazaAplicatia(){
         acceseaza = findViewById(R.id.button_logare);
         acceseaza.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
- //       String pin=parola.getText().toString().trim();
-                 Boolean res = myDb.verificPin(parola.getText().toString());
+                //       String pin=parola.getText().toString().trim();
+                Boolean res = myDb.verificPin(parola.getText().toString());
 
-            if (res == true) {
-                    Toast.makeText(MainActivity.this, " Bun venit "+ numeUtilizator()+ "  id:  " + id_Utilizator(), Toast.LENGTH_LONG).show();
+                if (res == true) {
+                    Toast.makeText(MainActivity.this, " Bun venit " + numeUtilizator(), Toast.LENGTH_LONG).show();
                     Intent logareUtilizator = new Intent(MainActivity.this, SelectieInitialaActivity.class);
-                    String idUtilizator=id_Utilizator();
+                    String idUtilizator = id_Utilizator();
                     logareUtilizator.putExtra("UTILIZATOR", idUtilizator);
+                    int iDpuncTlucrU =id_Punct_lucru();
+                    logareUtilizator.putExtra("ID_PUNCT_LUCRU", iDpuncTlucrU);
                     startActivity(logareUtilizator);
+                } else {
+                    Toast.makeText(MainActivity.this, " Utilizator neidentificat", Toast.LENGTH_SHORT).show();
                 }
-            else{
-                Toast.makeText(MainActivity.this, " Utilizator neidentificat", Toast.LENGTH_SHORT).show();
+
+
             }
+        });
+    }
 
 
-        }
-    });
-}
-    public String numeUtilizator(){
+    public String numeUtilizator() {
         SQLiteDatabase db = myDb.getReadableDatabase();
-        String result ="";
+        String result = "";
 
-        String queryUtilizator =( "select "+ Constructor.TabelaUtilizatorPin.COL_NUME + " from " + Constructor.TabelaUtilizatorPin.NUME_TABEL +
-                " where " + Constructor.TabelaUtilizatorPin.COL_PIN +" = " + parola.getText().toString().trim() );
+        String queryUtilizator = ("select " + Constructor.TabelaUtilizatorPin.COL_NUME + " from " + Constructor.TabelaUtilizatorPin.NUME_TABEL +
+                " where " + Constructor.TabelaUtilizatorPin.COL_PIN + " = " + parola.getText().toString().trim());
         Cursor cursor = db.rawQuery(queryUtilizator, null);
         if (cursor.moveToFirst()) {
             result = cursor.getString(cursor.getColumnIndex(Constructor.TabelaUtilizatorPin.COL_NUME));
@@ -69,18 +90,36 @@ public class MainActivity extends AppCompatActivity {
         cursor.close();
         return result;
     }
-    public String id_Utilizator(){
-        SQLiteDatabase db = myDb.getReadableDatabase();
-        String result ="";
 
-        String queryUtilizator =( "select "+ Constructor.TabelaUtilizatorPin.COL_ID + " from " + Constructor.TabelaUtilizatorPin.NUME_TABEL +
-                " where " + Constructor.TabelaUtilizatorPin.COL_PIN +" = " + parola.getText().toString().trim() );
+    public String id_Utilizator() {
+        SQLiteDatabase db = myDb.getReadableDatabase();
+        String result = "";
+
+        String queryUtilizator = ("select " + Constructor.TabelaUtilizatorPin.COL_ID + " from " + Constructor.TabelaUtilizatorPin.NUME_TABEL +
+                " where " + Constructor.TabelaUtilizatorPin.COL_PIN + " = " + parola.getText().toString().trim());
         Cursor cursor = db.rawQuery(queryUtilizator, null);
         if (cursor.moveToFirst()) {
             result = cursor.getString(cursor.getColumnIndex(Constructor.TabelaUtilizatorPin.COL_ID));
         }
         cursor.close();
         return result;
+    }
+
+    public int id_Punct_lucru(){
+        SQLiteDatabase db=myDb.getReadableDatabase();
+        final String oop=punctLucru.getText().toString();
+        int id_P_Lucru= LogicaVerificari.getPunctLucru(db,oop);
+        parola = findViewById(R.id.editTextPIN);
+        return id_P_Lucru;
+    }
+
+
+    public void PopulareAutocomplete() {
+        SQLiteDatabase db = myDb.getReadableDatabase();
+        final String[] Punct_Lucru = LogicaVerificari.getPlucru(db);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, support_simple_spinner_dropdown_item, Punct_Lucru);
+        punctLucru.setAdapter(adapter);
+
 
     }
 }
